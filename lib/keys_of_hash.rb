6cc7a10985501b489efe9cb1b42f34f2e2cfb046
Madeline Celis <madelinebcelis@gmail.com>
2018-07-26 02:08:19 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,18 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
+# {baseball: 7, basketball: 9}.keys_of("basketball")
+  def keys_of(*arguments)
+    newArray = []
+    arguments.each do |arg|
+      self.each do |key, value| #it should work with just the word each because it's implied taht you are running it on itself
+        #if you don't put anything in front of each   ex -- each do |arg| would still work
+        if value == arg
+         newArray << key
+       end#end of if statement
+      end #end of self
+    end # end of arguments
+    newArray
+  end #key_of method
+
 end
